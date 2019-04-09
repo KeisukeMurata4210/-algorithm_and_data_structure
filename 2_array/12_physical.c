5fe99a47eab7cbfadfd3438a7e79504fdeb68ccb
kmurata <kmurata@samurai-net.co.jp>
2019-04-09 09:50:28 +0900

    探索に入った

--- 2_array/12_physical.c
+++ 2_array/12_physical.c
@@ -10,6 +10,26 @@ typedef struct {
   double vision; /* 視力 */
 } PhysCheck;
 
+/* 身長の平均値を求める */
+double ave_height(const PhysCheck dat[], int n)/* 構造体型の配列ね */
+{
+  int i;
+  double sum = 0;
+  for (i = 0; i < n; i++)
+    sum += dat[i].height;
+  return sum / n;
+}
+
+/* 視力の分布を求める */
+void dist_vision(const PhysCheck dat[], int n, int dist[])
+{
+  int i;
+  for (i = 0; i < VMAX; i++)
+    dist[i] = 0;
+  for (i = 0; i < n; i++) /* nは人数 */
+    if (dat[i].vision >= 0.0 && dat[i].vision <= VMAX / 10.0)
+      dist[(int)(dat[i].vision * 10)]++;
+}
 
 int main(void)
 {
@@ -27,7 +47,20 @@ int main(void)
   int vdist[VMAX];    /* 視力の分布 */
 
   puts("   身体検査一覧表　　　");
-  puts(" 氏名　　　身長　視力　");
-  puts("--------------------");
+  puts(" 氏名　　　       身長　視力　");
+  puts("---------------------------");
   for (i = 0; i < nx; i++)
+    printf("%-18.18s%4d%5.1f\n", x[i].name, x[i].height, x[i].vision);/* 構造体型の配列なら、要素にドット演算子でメンバにアクセスできる */
+  printf("\n平均身長：%5.1fcm\n", ave_height(x, nx));
+  dist_vision(x, nx, vdist);
+  printf("\n視力の分布\n");
+  for (i = 0; i < VMAX; i++)
+    printf("%3.1f~：%2d人\n", i / 10.0, vdist[i]);/* 視力を10倍として扱った後/10.0とすることで計算のし易さを確保しつつ小数点を表現する */
+  return 0;
 }
+/* 
+%fの数字について
+4.1の「4」は、「小数点も含めて4桁で表す」の意味
+     「1」は、「小数点以下の」桁数
+4.1だと「12.3」などになる
+ */
---
+++ 3_search/1_ssearch1.c
@@ -0,0 +1,10 @@
+/* 線形探索 */
+# include <stdio.h>
+# include <stdlib.h>
+
+
+int main(void)
+{
+  int i, nx, ky, idx;
+  
+}
