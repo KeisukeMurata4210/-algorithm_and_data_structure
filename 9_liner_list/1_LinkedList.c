1d66cad4dd891af6ba2dcecf159c3f50f315b972
kmurata <kmurata@samurai-net.co.jp>
2019-04-17 11:27:01 +0900

    休もう

--- 9_liner_list/1_LinkedList.c
+++ 9_liner_list/1_LinkedList.c
@@ -42,5 +42,48 @@ Node *Search(List *list, const Member *x,
 /* 先頭にノードを挿入 */
 void InsertFront(List *list, const Member *x)
 {
-  Node *ptr = list->
+  Node *ptr = list->head;
+  list->head = list->cnt = AllocNode();
+  SetNode(list->head, x, ptr);
+}
+
+/* 末尾にノードを挿入 */
+void InsertRear(List *list, const Member *x)
+{
+  if (list->head == NULL)/* 空であれば先頭に挿入する */
+    InsertFront(list, x);
+  else {
+    Node *ptr = list->head;
+    while (ptr->next != NULL)/* デフォルトは先頭を指しているから、nextがNULLつまり一番後ろにくるまで更新していく */
+      ptr = ptr->next;
+    ptr->next = list->crnt = AllocNode();
+    SetNode(ptr->next, x, NULL);/* 後続ポインタがどこも指さないよう、NULLを渡す */
+  }
+}
+
+/* 先頭ノードを削除 */
+void RemoveFront(List *list)
+{
+  if (list->head != NULL) {/* リストが空でない時のみ */
+    Node *ptr = list->head->next;/* 先頭ノードの後続ポインタ=２番目のノードへのポインタ */
+    free(list->head);/* 動的に領域を確保したから、freeで開放すれば削除になる */
+    list->head = list->crnt = ptr;/* 2番目が先頭になる ノード自体は後ろのポインタしか知らない */
+  }
+}
+
+/* 末尾ノードを削除 */
+void RemoveRear(List *list)
+{
+  if (list->head != NULL) {
+    if ((list->head)->next == NULL) /* ノードが一つだけなら先頭を削除 */
+      RemoveFront(list);
+    else {
+      Node *ptr = list->head;
+      Node *pre;
+      while (ptr->next != NULL) {
+        pre = ptr;
+        ptr = ptr->next;
+      }
+    }
+  }
 }
