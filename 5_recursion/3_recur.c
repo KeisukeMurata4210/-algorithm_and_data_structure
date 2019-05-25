0057e22b8803e2d2903b0f494e99834a618311f6
KeisukeMurata4210 <keisuke.murata4210@gmail.com>
2019-05-25 17:21:49 +0900

    Effective Rubyをサクッと写経して、それから戻ってこようかな

---
+++ 5_recursion/3_recur.c
@@ -0,0 +1,23 @@
+/* 再帰に対する理解を深めるための真に再帰的な関数 */
+#include <stdio.h>
+
+/* 真に再帰的な関数recur */
+void recur(int n)
+{
+  if (n > 0) {
+    recur(n - 1);
+    printf("%d\n", n);
+    recur(n - 2);
+  }
+}
+
+int main(void)
+{
+  int x;
+  printf("整数を入力せよ：");
+  scanf("%d", &x);
+  recur(x);
+  return 0;
+}
+
+/* 実行される順番に、上から下へ枝分かれさせて調べる　トップダウン解析 */
