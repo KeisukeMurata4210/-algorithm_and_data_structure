f05a74beab9645d2ffd5ec13f7b4d5aca2e53532
KeisukeMurata4210 <keisuke.murata4210@gmail.com>
2019-09-23 21:44:28 +0900

    ちょっとこれ以上考えると明日に響きそうだ

---
+++ 3_search/9_ChainHashTest.c
@@ -0,0 +1,10 @@
+/* チェインハッシュChainHashの利用例 */
+#include <stdio.h>
+#include "Member.h"
+#include "ChainHash.h"
+
+/* メニュー */
+typedef enum {
+  TERMINATE, ADD, DELETE, SEARCH, CLEAR, DUMP
+} Menu;
+
--- 3_search/ChainHash.c
+++ 3_search/ChainHash.c
@@ -34,20 +34,86 @@ int Initialize(ChainHash *h, int size)
 /* 探索 */
 Node *Search(const ChainHash *h, const Member *x)
 {
-  
+  int key = hash(x->no, h->size); /* 探索するデータのハッシュ値 */
+  Node *p = h->table[key];   /* 着目ノード */
+  while (p != NULL) {
+    if (p->data.no == x->no)
+      return p;
+    p = p->next;  /* 後続ノードに着目 */
+  }
+  return NULL;   /* 探索失敗 */
 }
 
 /* データの追加 */
-int Add(ChainHash *h, const Member *x);
+int Add(ChainHash *h, const Member *x)
+{
+  int key = hash(x->no, h->size);/* 追加するデータのハッシュ値 */
+  Node *p = h->table[key];  /* 着目ノード */
+  Node *temp;
+  while (p != NULL) {
+    if (p->data.no == x->no)
+      return 1; /* このキーは登録済み　追加失敗 */
+    p = p->next;
+  } /* このwhileは「登録されたかどうか」を調べている */
+  if ((temp = calloc(1, sizeof(Node))) == NULL)
+    return 2;
+  SetNode(temp, x, h->table[key]);/* 追加するノードに値を設定　‼︎先頭に割り込むということは、現状のハッシュ表の要素が後続ポインタになる！ */
+  h->table[key] = temp;  /* ハッシュ表の要素を追加したノードへのポインタに変更 */
+  return 0;
+}
 
 /* データの削除 */
-int Remove(ChainHash *h, const Member *x);
+int Remove(ChainHash *h, const Member *x)
+{
+  int key = hash(x->no, h->size); /* 削除するデータのハッシュ値 */
+  Node *p = h->table[key];  /* 着目ノード */
+  Node **pp = &h->table[key];/* 着目ノードへのポインタ */
+  while (p != NULL) {
+    if (p->data.no == x->no) {/* 見つけたら */
+      *pp = p->next;
+      free(p);
+      return 0;
+    }
+    pp = &p->next;
+    p = p->next;/* 後続ノードに着目 */
+  }
+  return 1;
+}
 
 /* ハッシュ表をダンプ */
-void Dump(const ChainHash *h);
+void Dump(const ChainHash *h)/* ダンプとはすべて表示すること */
+{
+  int i;
+  for (i = 0; i < h->size; i++) {
+    Node *p = h->table[i];
+    printf("%02d  ", i);
+    while (p != NULL) {
+      printf("→ %d（%s） ", p->data.no, p->data.name);
+      p = p->next;
+    }
+    putchar('\n');
+  }
+}
 
 /* 全データの削除 */
-void Clear(ChainHash *h);
+void Clear(ChainHash *h)
+{
+  int i;
+  for (i = 0; i < h->size; i++) {
+    Node *p = h->table[i];
+    while (p != NULL) {
+      Node *next = p->next;
+      free(p);
+      p = next;
+    }
+    h->table[i] = NULL;
+  }
+}
 
 /* ハッシュ表を後始末 */
-void Terminate(ChainHash *h);
+void Terminate(ChainHash *h)
+{
+  Clear(h); /* 全データを削除 */
+  free(h->table);/* ハッシュ表用配列の */
+  h->size = 0;
+}
